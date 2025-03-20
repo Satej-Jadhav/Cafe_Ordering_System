1dc50d6d286d17e25a0a0a4dc62057a88f16def3
deviil00 <adevvil05@gmail.com>
2025-03-20 14:06:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use('/api/users', userRoutes);
 app.use('/api', menuRoutes);
 
 // Database Connection
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
